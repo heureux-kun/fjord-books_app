65f3077edde4f9af198cc76d01d6582efb5b6986
umlautone uenishi <uenishi@umlautone.jp>
2021-08-28 06:47:59 +0900

    rubocopの指摘を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,15 +17,14 @@ class UsersController < ApplicationController
   end
 
   def update
-    
     respond_to do |format|
       if @user.update(user_rarams)
-        format.html { redirect_to @book, notice: t('devise.registrations.updated')}
-        format.html{ redirect_to @user, notice: t('') }
-        format.json{ render :show, status: :ok, location: @user }
+        format.html { redirect_to @book, notice: t('devise.registrations.updated') }
+        format.html { redirect_to @user, notice: t('') }
+        format.json { render :show, status: :ok, location: @user }
       else
-        format.html{ render :edit }
-        format.json{ render json: @user.errors, status: :unprocessable_entity }
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
