dbe840673724415560ee8ee410105d1b84e22f9d
umlautone uenishi <uenishi@umlautone.jp>
2021-08-30 01:19:48 +0900

    ActiveStorageのインストール、userコントローラーにストロングパラメーター設定、フォームにフィールド追加、viewページに画像を表示するタグを追加（まだ画像は表示されていない）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,14 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def update
+    @user.update user_params
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:image)
+  end
 end
