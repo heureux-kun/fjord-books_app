d71c7f5223622b13a7a956690540def427291a77
umlautone uenishi <uenishi@umlautone.jp>
2021-09-06 06:34:21 +0900

    relationshipsモデルとuserモデルににアソシエーションの記述

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,11 @@
 class Relationship < ApplicationRecord
+  # user_idと関連付けるので、belongs_to :user
+  belongs_to :user
+
+  # follow_idと関連付けるので、belongs_to :follow
+  # Followクラスという存在しないクラスを参照することを防ぐため、Userクラスであることを明示
+  belongs_to :follow, class_name: 'User'
+
+  validates :user_id, presence: true
+  validates :follow_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,28 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+  has_many :relationships, foreign_key: 'user_id'
+  # relationshipsテーブルのfollow_idを参考にして、followingsモデルにアクセスするための記述
+  # source: :follow→「relationshipsテーブルのfollow_idを参考にして、followingsモデルにアクセスしてね」
+  # 結果として、user.followings でフォローしているUser達を取得できる。
+  has_many :followings, through: :relationships, source: :follow
+
+  # reverse_of_relationshipsという名前でアソシエーション（relationshipsの逆方向。だけどrelationsipモデルの事）外部キーはfollow_id
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, througt: relationships, source: user
+
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
 end
