99475ac50d1840520a8a1c43b8bf3cf28a8685c7
umlautone uenishi <uenishi@umlautone.jp>
2021-09-11 18:55:08 +0900

    不要なルーティングとファイルを削除、i18n対応できていなかった箇所を修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,4 @@ Rails.application.routes.draw do
   resources :books
   resources :users, :only => [:show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'user_registration_path', to: 'user#index'
 end
