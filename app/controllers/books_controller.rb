a0f7f63de2cb3e07024f3b1ed12d584fe4f9a477
umlautone uenishi <uenishi@umlautone.jp>
2021-09-16 00:20:42 +0900

    不要なファイルと記述を削除

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -42,10 +42,8 @@ class BooksController < ApplicationController
     respond_to do |format|
       if @book.update(book_params)
         format.html { redirect_to @book, notice: t('controllers.common.notice_update', name: Book.model_name.human) }
-        format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -56,7 +54,6 @@ class BooksController < ApplicationController
     @book.destroy
     respond_to do |format|
       format.html { redirect_to books_url, notice: t('controllers.common.notice_destroy', name: Book.model_name.human) }
-      format.json { head :no_content }
     end
   end
 
