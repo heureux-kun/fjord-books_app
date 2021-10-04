66e577d8f6f4c27549f32d35717901766e7fa94f
umlautone uenishi <uenishi@umlautone.jp>
2021-10-05 00:31:50 +0900

    不要なコメントを削除、タイトルが重複していた箇所を修正、パーシャルファイルではローカル変数を使うよう修正

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -8,7 +8,6 @@ class Relationship < ApplicationRecord
   # Followクラスという存在しないクラスを参照することを防ぐため、Userクラスであることを明示
   belongs_to :follow, class_name: 'User'
 
-  # MEMO：uniquenessはどちらにも書かなくてもOK？
   validates :user_id, presence: true
   validates :follow_id, presence: true, uniqueness: { scope: :user }
 end
